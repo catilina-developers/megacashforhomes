3d0330bf98de5297eddbba7227833627940c26ff
Hrithik Raj <hrithikraj0001@gmail.com>
2021-03-09 12:04:39 +0530

    debug = false

--- megacashforhomes/settings.py
+++ megacashforhomes/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8omho1cv1^zdlx8$*v5hn3))(ub(l4bm!rw_n3wzhps@!#8sj#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['megacashforhomes.herokuapp.com', '127.0.0.1', 'megacashforhomes.com', 'megacash4homes.com','www.megacashforhomes.com']

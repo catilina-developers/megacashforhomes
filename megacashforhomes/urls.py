9ed5d40a19217ea3067689057e6c9f449ece08ce
Hrithik Raj <hrithikraj0001@gmail.com>
2021-03-09 13:34:39 +0530

    debug urls

--- megacashforhomes/urls.py
+++ megacashforhomes/urls.py
@@ -43,5 +43,6 @@ urlpatterns = [
 
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 handler404 = 'acc.views.error_404_view'
